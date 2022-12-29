ffc363429d25cdcbc5a7aac47c6a56330b0d5ff8
Rodrigo Miguel <rodrigosmig@gmail.com>
2022-12-29 14:39:33 -0300

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
